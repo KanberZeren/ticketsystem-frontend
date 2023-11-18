00e1513aaa15a638d5b60e9bdf62e5d00a177759
Emre-Sag <emre.sag@student.htw-berlin.de>
2023-11-18 19:27:54 +0100

    HelloWorld.vue geändert in HomePage.vue (Welcome to our Ticketsystem)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Hello World!"/>
+    <HomePage msg="Welcome to our Ticketsystem"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomePage from '@/components/HomePage.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
